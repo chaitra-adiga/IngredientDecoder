198a889137f212e5ca2c844e384a001d84a67d1b
Chaitra Adiga <itsca03@gmail.com>
2025-02-23 11:02:43 +0530

    .env updated

--- ingredient_decoder/settings.py
+++ ingredient_decoder/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,10 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u@f*y8h$yp6vp@zg^mm_d+$q5g-vged9li$i5@!ky+z00^-u+4'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
+API_KEY = config('API_KEY')
 
 ALLOWED_HOSTS = []
 
@@ -76,11 +77,11 @@ WSGI_APPLICATION = 'ingredient_decoder.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "Ingredient_Decoder",  # The name of the database you created
-        "USER": "postgres",  # The username for your PostgreSQL database
-        "PASSWORD": "1234",  # The password you set during PostgreSQL installation
-        "HOST": "localhost",  # Or your database server's IP address
-        "PORT": "5432",  # Default PostgreSQL port
+        "NAME": config('DATABASE_NAME'),  # The name of the database you created
+        "USER": config('DATABASE_USER'),  # The username for your PostgreSQL database
+        "PASSWORD": config('DATABASE_PASSWORD'),  # The password you set during PostgreSQL installation
+        "HOST": config('DATABASE_HOST', default='localhost'),  # Or your database server's IP address
+        "PORT": config('DATABASE_PORT', default='5432'),  # Default PostgreSQL port
     }
 }
 
